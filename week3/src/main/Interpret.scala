6f4619f25c9cf9693d5dd972929d3495c53fb3a7
vladpaun1 <vlad.paun06@gmail.com>
2026-02-17 21:12:56 +0100

    adjust for new env

--- week3/src/main/Interpret.scala
+++ week3/src/main/Interpret.scala
@@ -1,26 +1,27 @@
+
 object Interpret {
 
-  def interp(e: ExprC): Value = e match {
+  def interp(e: ExprC, nv: Environment): Value = e match {
     case NumC(value) => NumV(value)
     case TrueC() => BoolV(true)
     case FalseC() => BoolV(false)
 
-    case AddC(l, r) => numBinOp(l, r)(_ + _)
-    case MulC(l, r) => numBinOp(l, r)(_ * _)
+    case AddC(l, r) => numBinOp(l,r,nv)(_ + _)
+    case MulC(l, r) => numBinOp(l,r,nv)(_ * _)
 
-    case EqNumC(l, r) => numCmpOp(l, r)(_ == _)
-    case LtNumC(l, r) => numCmpOp(l, r)(_ < _)
+    case EqNumC(l, r) => numCmpOp(l,r,nv)(_ == _)
+    case LtNumC(l, r) => numCmpOp(l,r,nv)(_ < _)
 
-    case IfC(c, t, f) => if asBool(interp(c)) then interp(t) else interp(f)
+    case IfC(c, t, f) => if asBool(interp(c,nv)) then interp(t,nv) else interp(f,nv)
     case UndefinedC() => throw InterpException("Undefined behavior")
 
     case NilC() => NilV()
-    case ConsC(head, tail) => ConsV(interp(head), interp(tail))
+    case ConsC(head, tail) => ConsV(interp(head,nv), interp(tail,nv))
 
-    case HeadC(e) => head(interp(e))
-    case TailC(e) => tail(interp(e))
-    case IsNilC(e) => BoolV(isNil(interp(e)))
-    case IsListC(e) => BoolV(isList(interp(e)))
+    case HeadC(e) => head(interp(e,nv))
+    case TailC(e) => tail(interp(e,nv))
+    case IsNilC(e) => isNil(interp(e,nv))
+    case IsListC(e) => isList(interp(e,nv))
   }
 
   private def asNum(v: Value): Int = v match {
@@ -43,18 +44,17 @@ object Interpret {
     case _ => throw InterpException("Expected a Cons")
   }
 
-  private def isNil(v: Value): Boolean = v match {
-    case NilV() => true
-    case ConsV(_, _) => false
+  private def isNil(v: Value): Value = v match {
+    case NilV() => BoolV(true)
+    case ConsV(_, _) => BoolV(false)
     case _ => throw InterpException("Not a list")
   }
 
-  private def isList(v: Value): Boolean = v match {
-    case NilV() => true
-    case ConsV(_, _) => true
-    case _ => false
+  private def isList(v: Value): Value = v match {
+    case NilV() | ConsV(_, _) => BoolV(true)
+    case _ => BoolV(false)
   }
 
-  private def numBinOp(l: ExprC, r: ExprC)(op: (Int, Int) => Int): Value = NumV(op(asNum(interp(l)), asNum(interp(r))))
-  private def numCmpOp(l: ExprC, r: ExprC)(op: (Int, Int) => Boolean): Value = BoolV(op(asNum(interp(l)), asNum(interp(r))))
+  private def numBinOp(l: ExprC, r: ExprC, nv: Environment)(op: (Int, Int) => Int): Value = NumV(op(asNum(interp(l,nv)), asNum(interp(r,nv))))
+  private def numCmpOp(l: ExprC, r: ExprC, nv: Environment)(op: (Int, Int) => Boolean): Value = BoolV(op(asNum(interp(l,nv)), asNum(interp(r,nv))))
 }
