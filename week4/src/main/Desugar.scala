cf0476e49f38a7b8ec2ecdd4dd316f9a56d71302
vladpaun1 <vlad.paun06@gmail.com>
2026-03-04 16:20:05 +0100

    almost finished week 4

--- week4/src/main/Desugar.scala
+++ week4/src/main/Desugar.scala
@@ -1,4 +1,53 @@
 object Desugar {
 
-  def desugar(e: ExprExt): ExprC = ???
+  def desugar(e: ExprExt): ExprC = e match {
+        case NumExt(value) => NumC(value)
+        case TrueExt() => TrueC()
+        case FalseExt() => FalseC()
+
+        case NegExt(e) => MulC(NumC(-1), desugar(e))
+        case AddExt(l, r) => AddC(desugar(l), desugar(r))
+        case SubExt(l, r) => AddC(desugar(l), MulC(NumC(-1), desugar(r)))
+        case MulExt(l, r) => MulC(desugar(l), desugar(r))
+
+        case EqNumExt(l, r) => EqNumC(desugar(l), desugar(r))
+        case LtNumExt(l, r) => LtNumC(desugar(l), desugar(r))
+        case GtNumExt(l, r) => LtNumC(desugar(r), desugar(l))
+
+        case NotExt(e) => IfC(desugar(e), FalseC(), TrueC())
+        case AndExt(l, r) => IfC(desugar(l), desugar(r), FalseC())
+        case OrExt(l, r) => IfC(desugar(l), TrueC(), desugar(r))
+
+        case IfExt(c, t, f) => IfC(desugar(c), desugar(t), desugar(f))
+        case CondExt(branches: List[CondBranch]) =>
+        branches.foldRight(UndefinedC(): ExprC) {
+            case (CondBranch(c, b), tail) => IfC(desugar(c), desugar(b), tail)
+        }
+        case CondEExt(branches, e) =>
+        branches.foldRight(desugar(e)) {
+            case (CondBranch(c, b), tail) => IfC(desugar(c), desugar(b), tail)
+        }
+
+        case NilExt() => NilC()
+        case ConsExt(head, tail) => ConsC(desugar(head), desugar(tail))
+        case ListExt(values) => values.map(desugar).foldRight(NilC(): ExprC)(ConsC.apply)
+
+        case HeadExt(e) => HeadC(desugar(e))
+        case TailExt(e) => TailC(desugar(e))
+        case IsNilExt(e) => IsNilC(desugar(e))
+        case IsListExt(e) => IsListC(desugar(e))
+
+        case IdExt(name) => IdC(name)
+        case LambdaExt(params, body) => LambdaC(params, desugar(body))
+        case AppExt(callee, args) => AppC(desugar(callee), args.map(desugar))
+        case LetExt(binds, body) => AppC(LambdaC(binds.map(_.name), desugar(body)),binds.map(b => desugar(b.value)))
+    
+        case SetExt(name, value) => SetC(name, desugar(value))
+        case SequenceExt(exprs) => exprs.map(desugar).reduceRight(SeqC.apply)
+        case LetRecExt(binds, body) => AppC(LambdaC(binds.map(_.name), binds.map(b => SetC(b.name, desugar(b.value))).foldRight(desugar(body): ExprC)(SeqC.apply: ExprC)), binds.map(_ => UninitialisedC()))
+
+        case BoxExt(e) => BoxC(desugar(e))
+        case UnboxExt(b) => UnboxC(desugar(b))
+        case SetBoxExt(b,e) => SetBoxC(desugar(b),desugar(e))
+    }
 }
--- week4/src/main/Interpret.scala
+++ week4/src/main/Interpret.scala
@@ -1,4 +1,124 @@
 object Interpret {
 
-  def interp(e: ExprC, nv: PointerEnvironment, st: Store): (Value, Store) = ???
+  def interp(e: ExprC, nv: PointerEnvironment, st: Store): (Value, Store) = e match {
+      case NumC(value) => (NumV(value), st)
+      case TrueC() => (BoolV(true), st)
+      case FalseC() => (BoolV(false), st)
+
+      case AddC(l, r) => numBinOp(l,r,nv,st)(_ + _)
+      case MulC(l, r) => numBinOp(l,r,nv,st)(_ * _)
+
+      case EqNumC(l, r) => numCmpOp(l,r,nv,st)(_ == _)
+      case LtNumC(l, r) => numCmpOp(l,r,nv,st)(_ < _)
+
+      case IfC(c, t, f) => {
+          val (cv, st2) = interp(c,nv,st)
+          if asBool(cv) then interp(t,nv,st2) else interp(f,nv,st2)
+      }
+      case UndefinedC() => throw InterpException("Undefined behavior")
+
+      case NilC() => (NilV(),st)
+      case ConsC(head, tail) => {
+          val (headV, st2) = interp(head,nv,st)
+          val (tailV, st3) = interp(tail,nv,st2)
+          (ConsV(headV,tailV), st3)
+      }
+
+      case HeadC(e) => {
+          val (v, st2) = interp(e, nv, st)
+          (head(v), st2)
+      }
+
+      case TailC(e) => {
+          val (v, st2) = interp(e, nv, st)
+          (tail(v), st2)
+      }
+
+      case IsNilC(e) => {
+          val (v, st2) = interp(e, nv, st)
+          (isNil(v), st2)
+      }
+
+      case IsListC(e) => {
+          val (v, st2) = interp(e, nv, st)
+          (isList(v), st2)
+      }
+
+      case IdC(name) => (fetch(lookup(name, nv),st), st)
+      case f:LambdaC => PointerClosureV(f,nv)
+      case AppC(callee: ExprC, args: List[ExprC]) => {
+        val (fv, st2) = interp(callee,nv,st)
+        fv match {
+          case PointerClosureV(LambdaC(params:List[String], body), enclosedEnv) => {
+              //maybe check arity of params and args
+
+              //interpret all args
+              val (interpedArg, st3) = interp(arg, nv, st2);
+              
+              //enclose all params
+              val newPointer = newLoc(st2)
+              val extendedEnv = (param, newPointer) :: enclosedEnv
+              val extendedStore = extendStore((newPointer, interpedArg), st3)
+
+              //interpret body
+              interp(body, extendedEnv, extendedStore)
+          }
+          case _ => throw InterpException("")
+        }
+      }
+  }
+
+  private def asNum(v: Value): Int = v match {
+    case NumV(n) => n
+    case _ => throw InterpException("Not a number")
+  }
+
+  private def asBool(v: Value): Boolean = v match {
+    case BoolV(b) => b
+    case _ => throw InterpException("Not a boolean")
+  }
+
+  private def head(v: Value): Value = v match {
+    case ConsV(h, _) => h
+    case _ => throw InterpException("Expected a Cons")
+  }
+
+  private def tail(v: Value): Value = v match {
+    case ConsV(_, t) => t
+    case _ => throw InterpException("Expected a Cons")
+  }
+
+  private def isNil(v: Value): Value = v match {
+    case NilV() => BoolV(true)
+    case ConsV(_, _) => BoolV(false)
+    case _ => throw InterpException("Not a list")
+  }
+
+  private def isList(v: Value): Value = v match {
+    case NilV() | ConsV(_, _) => BoolV(true)
+    case _ => BoolV(false)
+  }
+
+  private def numBinOp(l: ExprC, r: ExprC, nv: PointerEnvironment, st1: Store)(op: (Int, Int) => Int): (Value, Store) = {
+    val (lv, st2) = interp(l,nv,st1)
+    val (rv, st3) = interp(r,nv,st2)
+    (NumV(op(asNum(lv),asNum(rv))), st3)
+  }
+  private def numCmpOp(l: ExprC, r: ExprC, nv: PointerEnvironment, st1: Store)(op: (Int, Int) => Boolean): (Value, Store) = {
+    val (lv, st2) = interp(l, nv, st1)
+    val (rv, st3) = interp(r, nv, st2)
+    (BoolV(op(asNum(lv), asNum(rv))), st3)
+  }
+
+  def add(myEnv: PointerEnvironment, name: String, value : Value): PointerEnvironment = (name, value) :: myEnv
+  def take(myEnv: PointerEnvironment, name: String): Value = myEnv.collectFirst { case (`name`, value) => value }.getOrElse(throw MissingBindingException())
+ 
+  case class MissingBindingException() extends InterpException("No matching binding was found in the environmnet")
+  
+  def lookup(name: String, nv: PointerEnvironment): Pointer = nv.collectFirst {case (`name`, value) => value}.getOrElse(throw InterpException("goo goo gaa gaa"))
+  def update(loc: Pointer, st: Store, v: Value): Store = st.updated(if st.indexWhere((p,_) => p == loc) == -1 then throw InterpException("") else st.indexWhere((p,_) => p == loc), (loc,v))
+  def fetch(loc: Pointer, st: Store): Value = st.collectFirst {case (`loc`, value) => value}.getOrElse(throw InterpException("gaa gaa goo goo"))
+  def newLoc(store: Store): Pointer = Pointer(store.map {case (Pointer(i), _) => i}.maxOption.getOrElse(-1) + 1)
+  def extendStore(b: (Pointer, Value), st: Store): Store = st :+ b
+
 }
